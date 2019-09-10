5c607899db7c5caa26cdc17d3e00633b3a4fdc4b
GrenderG <grenderg@gmail.com>
2019-09-11 00:22:01 +0200

    Updated db dump and starting to add db updates.

---
+++ Database/Updates/updates.sql
@@ -0,0 +1,6 @@
+-- 10/09/2019
+-- Changing displayids for the correct ones
+update item_template set displayid = 7601 where entry = 1251; -- Linen Bandage
+update item_template set displayid = 7588 where entry = 2581; -- Heavy Linen Bandage
+update item_template set displayid = 9430 where entry = 3530; -- Wool Bandage
+update item_template set displayid = 7598 where entry = 3531; -- Heavy Wool Bandage
