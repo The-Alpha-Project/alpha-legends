9dc90620e1fb117f203289b775eedc9ead60861c
GrenderG <grenderg@gmail.com>
2019-09-26 03:29:43 +0200

    Item displayid fixes.

--- Database/Updates/updates.sql
+++ Database/Updates/updates.sql
@@ -1,8 +1,11 @@
 delimiter $
 begin not atomic
-	-- 26/09/2019
-	if (select count(*) from applied_updates where id='260920191') = 0 then
-		
+	-- 26/09/2019 2
+	if (select count(*) from applied_updates where id='260920192') = 0 then
+		update item_template set display_id = 926 where entry = 8766;
+		update item_template set display_id = 2209 where entry = 17184;
+
+		insert into applied_updates values('260920192');
 	end if;
 end $
 delimiter ;
