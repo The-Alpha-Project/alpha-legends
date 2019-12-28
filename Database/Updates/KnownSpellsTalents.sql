b8bb96600ffa8c05e16ef3341843e08b060a3062
GrenderG <grenderg@gmail.com>
2019-12-28 16:02:14 +0100

    Enhanced knownspells and knowntalents db structure

--- Database/Updates/KnownSpellsTalents.sql
+++ Database/Updates/KnownSpellsTalents.sql
@@ -1,47 +1,38 @@
-/*
-Navicat MariaDB Data Transfer
-
-Source Server         : MariaDB
-Source Server Version : 100411
-Source Host           : localhost:3306
-Source Database       : alpha_world
-
-Target Server Type    : MariaDB
-Target Server Version : 100411
-File Encoding         : 65001
-
-Date: 2019-12-28 09:14:10
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
 
--- ----------------------------
--- Table structure for knownspells
--- ----------------------------
+-- -----------------------------------------------------
+-- Table `knownspells`
+-- -----------------------------------------------------
 DROP TABLE IF EXISTS `knownspells`;
 CREATE TABLE `knownspells` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `guid` int(11) unsigned NOT NULL DEFAULT 0 COMMENT 'Global Unique Identifier',
-  `Spell` mediumint(8) unsigned NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1360 DEFAULT CHARSET=latin1;
+  `spell` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT 0,
+  `guid` INT(11) UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`spell`, `guid`),
+  INDEX `fk_knownspells_characters1_idx` (`guid` ASC),
+  CONSTRAINT `fk_knownspells_characters1`
+    FOREIGN KEY (`guid`)
+    REFERENCES `characters` (`guid`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
 
--- ----------------------------
--- Records of knownspells
--- ----------------------------
 
--- ----------------------------
--- Table structure for knowntalents
--- ----------------------------
+-- -----------------------------------------------------
+-- Table `knowntalents`
+-- -----------------------------------------------------
 DROP TABLE IF EXISTS `knowntalents`;
 CREATE TABLE `knowntalents` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `guid` int(11) unsigned NOT NULL DEFAULT 0 COMMENT 'Global Unique Identifier',
-  `Talent` mediumint(8) unsigned NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1354 DEFAULT CHARSET=latin1;
+  `talent` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT 0,
+  `guid` INT(11) UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`talent`, `guid`),
+  INDEX `fk_knowntalents_characters1_idx` (`guid` ASC),
+  CONSTRAINT `fk_knowntalents_characters1`
+    FOREIGN KEY (`guid`)
+    REFERENCES `characters` (`guid`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
 
--- ----------------------------
--- Records of knowntalents
--- ----------------------------
 SET FOREIGN_KEY_CHECKS=1;
